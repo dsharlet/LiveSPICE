263cf5a02fcab73716579891b5f0dce53855d8e0
Dillon Sharlet <dsharlet@gmail.com>
2013-10-01 23:19:24 -0700

    Broke Solve into building blocks

--- SyMath/Extensions/Solve.cs
+++ SyMath/Extensions/Solve.cs
@@ -11,17 +11,24 @@ namespace SyMath
     public static class SolveExtension
     {
         /// <summary>
-        /// Solve a linear equation or system of linear equations.
+        /// Compute the linear combinations of x that describe f.
         /// </summary>
-        /// <param name="f">Equation or set of equations to solve.</param>
-        /// <param name="x">Variable of set of variables to solve for.</param>
+        /// <param name="f"></param>
+        /// <param name="x"></param>
         /// <returns></returns>
-        public static List<Arrow> Solve(this IEnumerable<Equal> f, IEnumerable<Expression> x)
+        public static List<LinearCombination> TermsOf(this IEnumerable<Equal> f, IEnumerable<Expression> x)
         {
             // Convert f to a system of linear equations.
-            List<LinearCombination> S = f.Select(i => new LinearCombination(x, i.Left - i.Right)).ToList();
+            return f.Select(i => new LinearCombination(x, i.Left - i.Right)).ToList();
+        }
 
-            // Find row-echelon form of the system S.
+        /// <summary>
+        /// Compute the row-echelon form of the system S in terms of x. This function modifies S in place.
+        /// </summary>
+        /// <param name="S"></param>
+        /// <param name="x"></param>
+        public static void ToRowEchelon(this List<LinearCombination> S, IEnumerable<Expression> x)
+        {
             foreach (Expression PivotVariable in x)
             {
                 // Find the first row with i as a pivot variable.
@@ -33,8 +40,15 @@ namespace SyMath
                 foreach (LinearCombination j in S.Except(PivotRow).Where(i => PivotVariable.Equals(i.PivotVariable)))
                     j.AddScaled(-j.PivotCoefficient / PivotCoefficient, PivotRow);
             }
+        }
 
-            // Back substitution.
+        /// <summary>
+        /// Given a row-echelon form 
+        /// </summary>
+        /// <param name="S"></param>
+        /// <returns></returns>
+        public static void BackSubstitute(this List<LinearCombination> S)
+        {
             foreach (LinearCombination PivotRow in S.Where(i => !ReferenceEquals(i.PivotVariable, null)))
             {
                 Expression PivotVariable = PivotRow.PivotVariable;
@@ -44,7 +58,26 @@ namespace SyMath
                 foreach (LinearCombination r in S.Except(PivotRow).Where(i => !i[PivotVariable].IsZero()))
                     r.AddScaled(-r[PivotVariable] / PivotCoefficient, PivotRow);
             }
+        }
 
+        /// <summary>
+        /// Return the pivot variables of the system S.
+        /// </summary>
+        /// <param name="S"></param>
+        /// <returns></returns>
+        public static List<Expression> Pivots(this List<LinearCombination> S)
+        {
+            return S.Where(i => !ReferenceEquals(i.PivotVariable, null)).Select(i => i.PivotVariable).Distinct().ToList();
+        }
+
+        /// <summary>
+        /// Solve a system for the variables in x.
+        /// </summary>
+        /// <param name="S"></param>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static List<Arrow> Solve(this List<LinearCombination> S, IEnumerable<Expression> x)
+        {
             // Solve for the variables in x.
             List<Arrow> result = new List<Arrow>();
             foreach (Expression i in x)
@@ -61,6 +94,27 @@ namespace SyMath
                     result.Add(Arrow.New(i, row.Solve(i)));
             }
             return result;
+        }
+        
+        /// <summary>
+        /// Solve a linear equation or system of linear equations.
+        /// </summary>
+        /// <param name="f">Equation or set of equations to solve.</param>
+        /// <param name="x">Variable of set of variables to solve for.</param>
+        /// <returns></returns>
+        public static List<Arrow> Solve(this IEnumerable<Equal> f, IEnumerable<Expression> x)
+        {
+            // Convert f to a system of linear equations.
+            List<LinearCombination> S = f.TermsOf(x);
+
+            // Get row-echelon form of S.
+            S.ToRowEchelon(x);
+
+            // Back substitution.
+            S.BackSubstitute();
+
+            // Solve for the variables.
+            return S.Solve(x);
         }
     }
 }
