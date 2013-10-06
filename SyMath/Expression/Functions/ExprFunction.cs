a073d424aa7041ebf19c566431649445077200a1
Dillon Sharlet <dsharlet@gmail.com>
2013-10-06 00:40:30 -0700

    Added anonymous function definition

--- SyMath/Expression/Functions/ExprFunction.cs
+++ SyMath/Expression/Functions/ExprFunction.cs
@@ -20,6 +20,8 @@ namespace SyMath
 
         private ExprFunction(string Name, Expression Body, IEnumerable<Variable> Params) : base(Name) { body = Body; parameters = Params.ToList(); }
 
+        public static ExprFunction New(Expression Body, IEnumerable<Variable> Params) { return new ExprFunction("<Anonymous>", Body, Params); }
+        public static ExprFunction New(Expression Body, params Variable[] Params) { return new ExprFunction("<Anonymous>", Body, Params); }
         public static ExprFunction New(string Name, Expression Body, IEnumerable<Variable> Params) { return new ExprFunction(Name, Body, Params); }
         public static ExprFunction New(string Name, Expression Body, params Variable[] Params) { return new ExprFunction(Name, Body, Params); }
         public static ExprFunction New(string Name, IEnumerable<Variable> Params) { return new ExprFunction(Name, null, Params); }
