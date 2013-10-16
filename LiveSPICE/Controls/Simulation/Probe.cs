32991be731c3068c4a2dfc5ff8bfa80756cecfe4
Dillon Sharlet <dsharlet@gmail.com>
2013-10-15 22:27:04 -0700

    Moved node name

--- LiveSPICE/Controls/Simulation/Probe.cs
+++ LiveSPICE/Controls/Simulation/Probe.cs
@@ -44,7 +44,7 @@ namespace LiveSPICE
                 w - pw * 2);
 
             if (ConnectedTo != null)
-                Sym.DrawText(V.ToString(), new Point(10, 25), Alignment.Center, Alignment.Near);
+                Sym.DrawText(V.ToString(), new Point(3, 6), Alignment.Far, Alignment.Near);
         }
     }
 }
