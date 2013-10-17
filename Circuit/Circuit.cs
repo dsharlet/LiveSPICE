e2754b8fcdc50a8e19023c5bbcf56a0842164273
Dillon Sharlet <dsharlet@gmail.com>
2013-10-17 00:12:58 -0700

    Removed old code

--- Circuit/Circuit.cs
+++ Circuit/Circuit.cs
@@ -1,22 +1,16 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Xml;
-using System.Xml.Schema;
-using System.Xml.Serialization;
 using SyMath;
 
 namespace Circuit
-{
-    public class NonLinearCircuit : Exception
-    {
-    }
-        
+{        
     /// <summary>
     /// Circuits contain a list of nodes and components.
     /// </summary>
-    public class Circuit : Component, IXmlSerializable
+    public class Circuit : Component
     {
         private ComponentCollection components = new ComponentCollection();
         public ComponentCollection Components { get { return components; } }
@@ -34,17 +28,12 @@ namespace Circuit
                 return Components.OfType<Port>().Select(i => i.Terminal);
             }
         }
-
-        /// <summary>
-        /// Create a circuit from a SPICE netlist.
-        /// </summary>
-        /// <param name="Filename"></param>
-        /// <returns></returns>
-        public static Circuit FromNetlist(string Filename)
+        
+        public override void LayoutSymbol(SymbolLayout Sym)
         {
-            return Netlist.Parse(Filename);
+            throw new NotImplementedException();
         }
-
+        
         public override void Analyze(IList<Equal> Mna, IList<Expression> Unknowns)
         {
             foreach (Component c in Components)
@@ -67,32 +56,27 @@ namespace Circuit
                     Mna.Add(Equal.New(i, Constant.Zero));
             }
         }
-
-        /// <summary>
-        /// Get the single input port if there is exactly one input port. Throws otherwise.
-        /// </summary>
-        InputPort Input { get { return (InputPort)Components.Single(x => x is InputPort); } }
-        /// <summary>
-        /// Get the single output port if there is exactly one input port. Throws otherwise.
-        /// </summary>
-        OutputPort Output { get { return (OutputPort)Components.Single(x => x is OutputPort); } }
-
-        public override void LayoutSymbol(SymbolLayout Sym)
-        {
-            throw new NotImplementedException();
-        }
         
-        public void ReadXml(XmlReader Reader)
+        /// <summary>
+        /// Create a circuit from a SPICE netlist.
+        /// </summary>
+        /// <param name="Filename"></param>
+        /// <returns></returns>
+        public static Circuit FromNetlist(string Filename)
         {
+            return Netlist.Parse(Filename);
         }
 
-        public void WriteXml(XmlWriter Writer)
+        // Logging helpers.
+        private static void LogExpressions(ILog Log, string Title, IEnumerable<Expression> Expressions)
         {
-        }
-
-        public XmlSchema GetSchema()
-        {
-            return null;
+            if (Expressions.Any())
+            {
+                Log.WriteLine(MessageType.Info, Title);
+                foreach (Expression i in Expressions)
+                    Log.WriteLine(MessageType.Info, "  " + i.ToString());
+                Log.WriteLine(MessageType.Info, "");
+            }
         }
     };
 }
