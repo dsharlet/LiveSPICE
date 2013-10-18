640f987d1bbc4a8201a3506334f88082034a83ed
Dillon Sharlet <dsharlet@gmail.com>
2013-10-17 22:10:46 -0700

    Added global event handler for TextBox enter key

--- LiveSPICE/App.xaml.cs
+++ LiveSPICE/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Data;
 
 namespace LiveSPICE
 {
@@ -13,5 +16,21 @@ namespace LiveSPICE
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            EventManager.RegisterClassHandler(typeof(TextBox), TextBox.KeyDownEvent, new KeyEventHandler(TextBox_KeyDown));
+        }
+
+        void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            TextBox textbox = (TextBox)sender;
+            if (e.Key == Key.Enter & textbox.AcceptsReturn == false) 
+            {
+                BindingExpression be = textbox.GetBindingExpression(TextBox.TextProperty);
+                be.UpdateSource();
+
+                Window.GetWindow(textbox).Focus();
+            }
+        }
     }
 }
