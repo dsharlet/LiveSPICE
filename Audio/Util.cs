485203a75e98198c512b6e05d51d23547215caa6
Dillon Sharlet <dsharlet@gmail.com>
2013-10-26 03:46:14 -0700

    Fixed exceptions in debug mode

--- Audio/Util.cs
+++ Audio/Util.cs
@@ -9,13 +9,13 @@ namespace Audio
 {
     public static class Util
     {
-        [DllImport("NativeAudioUtils.dll")] public static extern void LEi16ToLEf32(IntPtr In, float[] Out, int Count);
-        [DllImport("NativeAudioUtils.dll")] public static extern void LEi16ToLEf64(IntPtr In, double[] Out, int Count);
-        [DllImport("NativeAudioUtils.dll")] public static extern void LEi32ToLEf32(IntPtr In, float[] Out, int Count);
-        [DllImport("NativeAudioUtils.dll")] public static extern void LEi32ToLEf64(IntPtr In, double[] Out, int Count);
-        [DllImport("NativeAudioUtils.dll")] public static extern void LEf32ToLEi16(float[] In, IntPtr Out, int Count);
-        [DllImport("NativeAudioUtils.dll")] public static extern void LEf64ToLEi16(double[] In, IntPtr Out, int Count);
-        [DllImport("NativeAudioUtils.dll")] public static extern void LEf32ToLEi32(float[] In, IntPtr Out, int Count);
-        [DllImport("NativeAudioUtils.dll")] public static extern void LEf64ToLEi32(double[] In, IntPtr Out, int Count);
+        [DllImport("NativeAudioUtils.dll", CallingConvention = CallingConvention.Cdecl)] public static extern void LEi16ToLEf32(IntPtr In, float[] Out, int Count);
+        [DllImport("NativeAudioUtils.dll", CallingConvention = CallingConvention.Cdecl)] public static extern void LEi16ToLEf64(IntPtr In, double[] Out, int Count);
+        [DllImport("NativeAudioUtils.dll", CallingConvention = CallingConvention.Cdecl)] public static extern void LEi32ToLEf32(IntPtr In, float[] Out, int Count);
+        [DllImport("NativeAudioUtils.dll", CallingConvention = CallingConvention.Cdecl)] public static extern void LEi32ToLEf64(IntPtr In, double[] Out, int Count);
+        [DllImport("NativeAudioUtils.dll", CallingConvention = CallingConvention.Cdecl)] public static extern void LEf32ToLEi16(float[] In, IntPtr Out, int Count);
+        [DllImport("NativeAudioUtils.dll", CallingConvention = CallingConvention.Cdecl)] public static extern void LEf64ToLEi16(double[] In, IntPtr Out, int Count);
+        [DllImport("NativeAudioUtils.dll", CallingConvention = CallingConvention.Cdecl)] public static extern void LEf32ToLEi32(float[] In, IntPtr Out, int Count);
+        [DllImport("NativeAudioUtils.dll", CallingConvention = CallingConvention.Cdecl)] public static extern void LEf64ToLEi32(double[] In, IntPtr Out, int Count);
     }
 }
