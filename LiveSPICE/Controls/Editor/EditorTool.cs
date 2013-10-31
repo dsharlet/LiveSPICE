bbf37c7c3204b1ffb24c64c2661a6a22395cd745
Dillon Sharlet <dsharlet@gmail.com>
2013-10-30 21:30:11 -0700

    Removed redundant escape handler

--- LiveSPICE/Controls/Editor/EditorTool.cs
+++ LiveSPICE/Controls/Editor/EditorTool.cs
@@ -20,16 +20,5 @@ namespace LiveSPICE
         public SchematicEditor Editor { get { return (SchematicEditor)Target; } }
 
         public EditorTool(SchematicEditor Target) : base(Target) { }
-
-        public override bool KeyDown(Key Key) 
-        {
-            switch (Key)
-            {
-                case System.Windows.Input.Key.Escape:
-                    Target.Tool = new SelectionTool(Editor); 
-                    return true;
-                default: return false;
-            }
-        }
     }
 }
