a9c91af4196353c5694a28a00bb84912198ccd3a
Dillon Sharlet <dsharlet@gmail.com>
2013-11-11 00:06:51 -0800

    Implemented factoring of common subexpressions

--- SyMath/Extensions/Factor.cs
+++ SyMath/Extensions/Factor.cs
@@ -42,28 +42,28 @@ namespace SyMath
                 List<Expression> terms = s.Terms.Select(i => i.Factor()).ToList();
                 
                 // All of the distinct factors, excluding constants.
-                List<Expression> factors = terms.SelectMany(i => Product.TermsOf(i).Where(j => !(j is Constant))).Distinct().ToList();
+                List<Expression> factors = terms.SelectMany(i => Product.TermsOf(i)).Distinct().ToList();
                 // Choose the most common factor to use.
                 Expression factor = factors.ArgMax(i => terms.Count(j => Product.TermsOf(j).Contains(i)));
                 List<Expression> contains = terms.Where(i => Product.TermsOf(i).Contains(factor)).ToList();
                 if (contains.Count > 1)
                     return Sum.New(
-                        Product.New(factor, Sum.New(contains.Select(i => Product.New(Product.TermsOf(i).Except(factor))))),
+                        Product.New(factor, Sum.New(contains.Select(i => Product.New(Product.TermsOf(i).Except(factor)))).Factor()),
                         Sum.New(terms.ExceptUnique(contains)).Factor()).Factor();
 
-                // Find the largest constant.
-                Real A = 1;
-                foreach (Expression i in terms)
-                {
-                    Real Ai = Product.TermsOf(i).OfType<Constant>().Select(j => j.Value).Aggregate((Real)1, (a, j) => a * j);
-                    if (Real.Abs(Real.Max(Ai, 1 / Ai)) > Real.Abs(Real.Max(A, 1 / A)))
-                        A = Ai;
-                    else if (Ai == -1 && A == 1)
-                        A = Ai;
-                }
+                //// Find the largest constant.
+                //Real A = 1;
+                //foreach (Expression i in terms)
+                //{
+                //    Real Ai = Product.TermsOf(i).OfType<Constant>().Select(j => j.Value).Aggregate((Real)1, (a, j) => a * j);
+                //    if (Real.Abs(Real.Max(Ai, 1 / Ai)) > Real.Abs(Real.Max(A, 1 / A)))
+                //        A = Ai;
+                //    else if (Ai == -1 && A == 1)
+                //        A = Ai;
+                //}
 
-                if (A != 1)
-                    return Product.New(Constant.New(A), Sum.New(terms.Select(i => i / A)));
+                //if (A != 1)
+                //    return Product.New(Constant.New(A), Sum.New(terms.Select(i => i / A)));
             }
 
             return f;
