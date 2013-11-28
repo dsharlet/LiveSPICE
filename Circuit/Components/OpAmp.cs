e4dc77650db9199a39fedca2bcc5ebe8ef8e7cd9
Dillon Sharlet <dsharlet@gmail.com>
2013-11-27 18:18:16 -0800

    New op-amp saturation model

--- Circuit/Components/OpAmp.cs
+++ Circuit/Components/OpAmp.cs
@@ -15,71 +15,58 @@ namespace Circuit
     [Description("Op-amp with model for input resistance Rin, output resistance Rout, and open-loop gain Aol. If the power supply terminals are connected, the op-amp will saturate.")]
     public class OpAmp : IdealOpAmp
     {
-        protected Terminal vpp, vnn;
-
-        public override IEnumerable<Terminal> Terminals
-        {
-            get
-            {
-                return base.Terminals.Append(vpp, vnn);
-            }
-        }
+        protected Terminal vcc, vee;
+                
+        public override IEnumerable<Terminal> Terminals { get { return base.Terminals.Append(vcc, vee); } }
 
         public OpAmp() 
         {
-            vpp = new Terminal(this, "Vcc+");
-            vnn = new Terminal(this, "Vcc-");
+            vcc = new Terminal(this, "Vcc+");
+            vee = new Terminal(this, "Vcc-");
         }
 
         protected Quantity rin = new Quantity(500e6, Units.Ohm);
-        [Description("Input resistance.")]
-        [Serialize]
+        [Serialize, Description("Input resistance.")]
         public Quantity Rin { get { return rin; } set { if (rin.Set(value)) NotifyChanged("Rin"); } }
 
-        protected Quantity rout = new Quantity(1e2m, Units.Ohm);
-        [Description("Output resistance.")]
-        [Serialize]
+        protected Quantity rout = new Quantity(75m, Units.Ohm);
+        [Serialize, Description("Output resistance.")]
         public Quantity Rout { get { return rout; } set { if (rout.Set(value)) NotifyChanged("Rout"); } }
 
         protected Quantity gain = new Quantity(1e5m, Units.None);
-        [Description("Open-loop gain.")]
-        [Serialize]
+        [Serialize, Description("Open-loop gain.")]
         public Quantity Aol { get { return gain; } set { if (gain.Set(value)) NotifyChanged("Aol"); } }
         
-        private static readonly Constant Pi = Constant.New(Math.PI);
-        private static readonly Constant Epsilon = Constant.New(1e-6);
-
-        private static Expression Clamp(Expression x, Expression a, Expression b)
-        {
-            return Call.Max(Call.Min(x, b), a);
-        }
-
-        //private static Expression Saturate(Expression x) { return Call.Abs(x) * x / (1 + x * x); }
-
-        private static Expression Saturate(Expression x) { return Call.ArcTan(Pi * x) / Pi; }
-        private static Expression InvSaturate(Expression x) { return Call.Tan(x * Pi) / Pi; }
-                        
         public override void Analyze(Analysis Mna)
         {
             // The input terminals are connected by a resistor Rin.
-            Resistor.Analyze(Mna, Positive, Negative, Rin);
+            Resistor.Analyze(Mna, Negative, Positive, Rin);
 
-            // Compute Vout using Vout = A*Vin
-            Expression Vout = (Expression)Aol * (Positive.V - Negative.V);
-            if (vpp.IsConnected && vnn.IsConnected)
+            // Compute Vout using Vout = Aol*V[Rin]
+            Expression Vout = (Expression)Aol * (Negative.V - Positive.V);
+            if (vcc.IsConnected && vee.IsConnected)
             {
-                // Saturate the output if the power supply terminals are connected.
-                Expression Vmax = vpp.V - vnn.V;
-                Expression Vmid = (vpp.V + vnn.V) / 2;
+                Node ncc = new Node() { Name = "ncc" };
+                Node nee = new Node() { Name = "nee" };
+                Node lim = new Node() { Name = "lim" };
 
-                Vout = Vmax * Saturate(Vout / Vmax) + Vmid;
-                Mna.AddTerminal(Out, (Vout - Out.V) / (Expression)Rout);
+                Mna.PushContext();
 
-                //Mna.AddEquation(Saturate(Vout / Vmax), (Out.V - Vmid) / Vmax);
-                //Mna.AddTerminal(Out, Mna.AddNewUnknown("i" + Name));
+                // Implement voltage limiter.
+                Mna.DeclNodes(ncc, nee, lim);
 
-                //Mna.AddEquation(Vout / Vmax, InvSaturate((Out.V - Vmid) / Vmax));
-                //Mna.AddTerminal(Out, Mna.AddNewUnknown("i" + Name));
+                VoltageSource.Analyze(Mna, vcc, ncc, 2);
+                Diode.Analyze(Mna, lim, ncc, 8e-16, 1, VT);
+
+                VoltageSource.Analyze(Mna, vee, nee, -2);
+                Diode.Analyze(Mna, nee, lim, 8e-16, 1, VT);
+
+                Mna.AddTerminal(lim, (Vout - lim.V) / (Expression)Rout);
+
+                // Output.
+                Conductor.Analyze(Mna, lim, Out);
+
+                Mna.PopContext();
             }
             else
             {
@@ -89,13 +76,7 @@ namespace Circuit
 
         public static void LayoutSymbol(SymbolLayout Sym, Terminal p, Terminal n, Terminal o, Terminal vp, Terminal vn, Func<string> Name, Func<string> Part)
         {
-            Sym.AddTerminal(p, new Coord(-20, -10));
-            Sym.DrawPositive(EdgeType.Black, new Coord(-15, -10));
-
-            Sym.AddTerminal(n, new Coord(-20, 10));
-            Sym.DrawNegative(EdgeType.Black, new Coord(-15, 10));
-
-            Sym.AddTerminal(o, new Coord(20, 0));
+            IdealOpAmp.LayoutSymbol(Sym, p, n, o, Name);
 
             Sym.AddTerminal(vn, new Coord(0, -20), new Coord(0, -10));
             Sym.DrawNegative(EdgeType.Black, new Coord(5, -13));
@@ -103,16 +84,10 @@ namespace Circuit
             Sym.AddTerminal(vp, new Coord(0, 20), new Coord(0, 10));
             Sym.DrawPositive(EdgeType.Black, new Coord(5, 13));
 
-            Sym.AddLoop(EdgeType.Black,
-                new Coord(-20, 20),
-                new Coord(-20, -20),
-                new Coord(20, 0));
-
             if (Part != null)
                 Sym.DrawText(Part, new Coord(12, 4), Alignment.Near, Alignment.Near);
-            Sym.DrawText(Name, new Coord(12, -4), Alignment.Near, Alignment.Far);
         }
 
-        public override void LayoutSymbol(SymbolLayout Sym) { LayoutSymbol(Sym, Positive, Negative, Out, vpp, vnn, () => Name, () => PartNumber); }
+        public override void LayoutSymbol(SymbolLayout Sym) { LayoutSymbol(Sym, Positive, Negative, Out, vcc, vee, () => Name, () => PartNumber); }
     }
 }
