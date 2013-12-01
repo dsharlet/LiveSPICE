6aff43e3b44e571bbcb6171fa58427a403e5b418
Dillon Sharlet <dsharlet@gmail.com>
2013-12-01 00:50:06 -0800

    Moved code to Tester class, added WriteDocs

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Reflection;
@@ -13,32 +14,30 @@ using Util;
 
 namespace Tests
 {
-    class Program
+    class Tester
     {
-        static readonly Variable t = Component.t;
+        private static readonly Variable t = Component.t;
 
-        static int SampleRate = 44100;
-        static int Samples = 100000;
-        static int Oversample = 8;
-        static int Iterations = 8;
+        public int SampleRate = 44100;
+        public int Samples = 100000;
+        public int Oversample = 8;
+        public int Iterations = 8;
 
-        static double analysisTime = 0.0;
-        static double simulateTime = 0.0;
+        private double analysisTime = 0.0;
+        private double simulateTime = 0.0;
 
-        static ConsoleLog Log = new ConsoleLog() { Verbosity = MessageType.Info };
+        public Log Log = new ConsoleLog() { Verbosity = MessageType.Info };
 
-        static void Main(string[] args)
+        public void Run(IEnumerable<string> Tests, Func<double, double> Vin)
         {
-            Func<double, double> Vin = t => Harmonics(t, 0.5, 82, 2);
-
             List<string> errors = new List<string>();
             List<string> performance = new List<string>();
 
             // This test generates the signal for the LiveSPICE 'logo'.
             //Run("BossSD1NoBuffer.xml", Vin, "V1[t]", new Expression[] { "_v15[t]", "_v11[t]" });
             //return;
-            
-            foreach (string File in System.IO.Directory.EnumerateFiles(@".", "*.schx"))
+
+            foreach (string File in Tests)
             {
                 string Name = System.IO.Path.GetFileNameWithoutExtension(File);
                 try
@@ -46,43 +45,43 @@ namespace Tests
                     double perf = Run(File, Vin);
                     performance.Add(Name + ":\t" + Quantity.ToString(perf, Units.Hz) + " (" + (perf / (double)SampleRate).ToString("G3") + "x real time)");
                 }
-                catch (Exception ex) 
+                catch (Exception ex)
                 {
                     errors.Add(Name + ":\t" + ex.Message);
-                    Console.WriteLine(ex.Message);
+                    Log.WriteLine(ex.Message);
                 }
             }
 
-            Console.WriteLine("Analyze/Simulate {0}/{1}", analysisTime, simulateTime);
+            Log.WriteLine("Analyze/Simulate {0}/{1}", analysisTime, simulateTime);
 
-            Console.WriteLine("{0} succeeded:", performance.Count);
+            Log.WriteLine("{0} succeeded:", performance.Count);
             foreach (string i in performance)
-                Console.WriteLine(i);
+                Log.WriteLine(i);
 
-            Console.WriteLine("{0} failed:", errors.Count);
+            Log.WriteLine("{0} failed:", errors.Count);
             foreach (string i in errors)
-                Console.WriteLine(i);
+                Log.WriteLine(i);
         }
 
-        public static double Run(string FileName, Func<double, double> Vin)
+        public double Run(string FileName, Func<double, double> Vin)
         {
             Circuit.Circuit C = Schematic.Load(FileName, Log).Build();
             C.Name = System.IO.Path.GetFileNameWithoutExtension(FileName);
             return Run(
-                C, 
-                Vin, 
-                C.Components.OfType<Input>().Select(i => Expression.Parse(i.Name + "[t]")).DefaultIfEmpty("V[t]").SingleOrDefault(), 
+                C,
+                Vin,
+                C.Components.OfType<Input>().Select(i => Expression.Parse(i.Name + "[t]")).DefaultIfEmpty("V[t]").SingleOrDefault(),
                 C.Nodes.Select(i => i.V));
         }
 
-        public static double Run(string FileName, Func<double, double> Vin, Expression Input, IEnumerable<Expression> Plots)
+        public double Run(string FileName, Func<double, double> Vin, Expression Input, IEnumerable<Expression> Plots)
         {
             Circuit.Circuit C = Schematic.Load(FileName, Log).Build();
             C.Name = System.IO.Path.GetFileNameWithoutExtension(FileName);
             return Run(C, Vin, Input, Plots);
         }
 
-        public static double Run(Circuit.Circuit C, Func<double, double> Vin, Expression Input, IEnumerable<Expression> Plots)
+        public double Run(Circuit.Circuit C, Func<double, double> Vin, Expression Input, IEnumerable<Expression> Plots)
         {
             long a = Timer.Counter;
 
@@ -90,9 +89,9 @@ namespace Tests
             TransientSolution TS = TransientSolution.Solve(analysis, (Real)1 / (SampleRate * Oversample), Log);
 
             analysisTime += Timer.Delta(a);
-            
+
             Simulation S = new LinqCompiledSimulation(TS, Oversample, Log);
-            Console.WriteLine("");
+            Log.WriteLine("");
             if (Samples > 0)
                 return RunTest(
                     C, S,
@@ -105,44 +104,49 @@ namespace Tests
                 return 0.0;
         }
 
-        public static double RunTest(Circuit.Circuit C, Simulation S, Expression Input, IEnumerable<Expression> Outputs, Func<double, double> Vin, int N, string Name)
-        {            
+        public double RunTest(Circuit.Circuit C, Simulation S, Expression Input, IEnumerable<Expression> Outputs, Func<double, double> Vin, int Samples, string Name)
+        {
             double t0 = (double)S.Time;
-            
-            Dictionary<Expression, double[]> input = new Dictionary<Expression, double[]>();
-            double[] vs = new double[N];
-            for (int n = 0; n < vs.Length; ++n)
-                vs[n] = Vin(n * S.TimeStep);
-            input.Add(Input, vs);
 
-            Dictionary<Expression, double[]> output = Outputs.ToDictionary(i => i, i => new double[vs.Length]);
-            
+            int N = 353;
+
+            Dictionary<Expression, double[]> input = new Dictionary<Expression, double[]>();
+            double[] vin = new double[N];
+            input.Add(Input, vin);
+
+            Dictionary<Expression, List<double>> output = Outputs.ToDictionary(i => i, i => new List<double>());
+            Dictionary<Expression, double[]> buffers = Outputs.ToDictionary(i => i, i => new double[N]);
+
             // Ensure that the simulation is cached before benchmarking.
-            S.Run(1, input, output, Iterations);
+            S.Run(1, input, buffers, Iterations);
             S.Reset();
 
             double time = 0.0;
-            try
+            int samples = 0;
+            double t = 0;
+            for (; samples < Samples; samples += N)
             {
+                for (int n = 0; n < N; ++n, t += S.TimeStep)
+                    vin[n] = Vin(t);
+
                 long a = Timer.Counter;
-                S.Run(vs.Length, input, output, Iterations);
-                time = Timer.Delta(a);
-                simulateTime += time;
-            }
-            catch (SimulationDiverged Ex)
-            {
-                if (Ex.At < 10) throw;
-                N = (int)Ex.At;
-            }
+                S.Run(N, input, buffers, Iterations);
+                time += Timer.Delta(a);
 
-            int t1 = Math.Min(N, 2000);
+                foreach (Expression key in output.Keys)
+                    output[key].AddRange(buffers[key]);
+            }
+            simulateTime += time;
 
-            Console.WriteLine("Performance {0}", Quantity.ToString(N / time, Units.Hz));
+            int t1 = Math.Min(Samples, 4000);
+
+            Log.WriteLine("Performance {0}", Quantity.ToString(samples / time, Units.Hz));
 
             Plot p = new Plot()
             {
-                Title = Name, 
-                Width = 800, Height = 400, 
+                Title = Name,
+                Width = 800,
+                Height = 400,
                 x0 = t0,
                 x1 = S.TimeStep * t1,
                 xLabel = "Time (s)",
@@ -152,11 +156,69 @@ namespace Tests
             p.Series.AddRange(output.Select(i => new Scatter(
                 i.Value.Take(t1)
                 .Select((j, n) => new KeyValuePair<double, double>(n * S.TimeStep, j)).ToArray()) { Name = i.Key.ToString() }));
-            return N / time;
+            return samples / time;
+        }
+    }
+
+    class Program
+    {
+        static void WriteDocs()
+        {
+            FileStream file = new FileStream("docs.html", FileMode.Create);
+            StreamWriter docs = new StreamWriter(file);
+
+            Action<StreamWriter, string, string, string> WriteTag = (StreamWriter S, string Tab, string Tag, string P) => S.WriteLine(Tab + "<" + Tag + ">" + P + "</" + Tag + ">");
+
+            docs.WriteLine("<section id=\"components\">");
+            docs.WriteLine("\t<h3>Components</h3>");
+            docs.WriteLine("\t<p>This section describes the operation of the basic component types provided in LiveSPICE. All of the components in the library are directly or indirectly (via subcircuits) implemented using these component types.</p>");
+
+            Type root = typeof(Circuit.Component);
+            foreach (Assembly i in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (Type j in i.GetTypes().Where(j => j.IsPublic && !j.IsAbstract && root.IsAssignableFrom(j)))
+                {
+                    try
+                    {
+                        System.ComponentModel.DisplayNameAttribute name = j.GetCustomAttribute<System.ComponentModel.DisplayNameAttribute>();
+                        System.ComponentModel.DescriptionAttribute desc = j.GetCustomAttribute<System.ComponentModel.DescriptionAttribute>();
+                        
+                        docs.WriteLine("\t<section id=\"" + j.Name + "\">");
+                        docs.WriteLine("\t<h4>" + (name != null ? name.DisplayName : j.Name) + "</h4>");
+                        if (desc != null)
+                            WriteTag(docs, "\t\t", "p", desc.Description);
+
+                        docs.WriteLine("\t\t<h5>Properties</h5>");
+                        docs.WriteLine("\t\t<ul>");
+                        foreach (PropertyInfo p in j.GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(k => k.GetCustomAttribute<Serialize>() != null))
+                        {
+                            desc = p.GetCustomAttribute<System.ComponentModel.DescriptionAttribute>();
+                            StringBuilder prop = new StringBuilder();
+                            prop.Append("<span class=\"property\">" + p.Name + "</span>");
+                            if (desc != null)
+                                prop.Append(": " + desc.Description);
+
+                            WriteTag(docs, "\t\t\t", "li", prop.ToString());
+                        }
+                        docs.WriteLine("\t\t</ul>");
+                    }
+                    catch (Exception) { }
+                }
+            }
+
+            docs.WriteLine("</section> <!-- components -->");
+        }
+
+        static void Main(string[] args)
+        {
+            //WriteDocs();
+
+            Tester tests = new Tester();
+            tests.Run(System.IO.Directory.EnumerateFiles(@".", "*.schx"), t => Harmonics(t, 0.5, 82, 2));
         }
 
         // Generate a function with the first N harmonics of f0.
-        static double Harmonics(double t, double A, double f0, int N)
+        private static double Harmonics(double t, double A, double f0, int N)
         {
             double s = 0;
             for (int i = 1; i <= N; ++i)
