bf1ea0f121ae520e2e9fec0bc5ef1c533b52f891
Dillon Sharlet <dsharlet@gmail.com>
2014-03-08 17:58:30 -0800

    Added dBConverter

---
+++ LiveSPICE/Utils/dBConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace LiveSPICE
+{
+    class dBConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 20 * Math.Log((double)value, 10);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Math.Pow(10, (double)value / 20);
+        }
+    }
+}
