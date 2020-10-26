7f3a08f4eb4604c7d700df147141b3aafdbbd734
Dillon <dsharlet@gmail.com>
2020-10-26 00:19:50 -0700

    More test cleanup

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,16 +1,52 @@
 ﻿using System;
 using System.IO;
+using Circuit;
+using ComputerAlgebra;
+using System.Collections.Generic;
+using Util;
+using System.Linq;
 
 namespace Tests
 {
     class Program
     {
+        // TODO: Make these command line arguments.
+        static int SampleRate = 48000;
+        static int Samples = 4800;
+        static int Oversample = 8;
+        static int Iterations = 8;
+
         static void Main(string[] args)
         {
-            //WriteDocs();
+            bool test = args.Contains("--test");
+            bool benchmark = args.Contains("--benchmark");
+            bool plot = args.Contains("--plot");
 
-            Test test = new Test();
-            test.Run(Directory.EnumerateFiles(@".", "*.schx"), t => Harmonics(t, 0.5, 82, 2));
+            Log log = new ConsoleLog() { Verbosity = MessageType.Info };
+            Test tester = new Test();
+
+            foreach (string i in args.Where(i => !i.StartsWith("--")))
+            {
+                foreach (string File in Globber.Glob(i))
+                {
+                    System.Console.WriteLine(File);
+                    Circuit.Circuit C = Schematic.Load(File, log).Build();
+                    C.Name = Path.GetFileNameWithoutExtension(File);
+                    if (test)
+                    {
+                        Dictionary<Expression, List<double>> outputs =
+                            tester.Run(C, t => Harmonics(t, 0.5, 82, 2), SampleRate, Samples, Oversample, Iterations);
+                        if (plot)
+                            tester.PlotAll(C.Name, outputs);
+                    }
+                    if (benchmark)
+                    {
+                        double rate = tester.Benchmark(C, t => Harmonics(t, 0.5, 82, 2), SampleRate, Oversample, Iterations);
+                        System.Console.WriteLine("{0:G3} kHz, {1:G3}x real time", rate / 1000, rate / SampleRate);
+                    }
+                    System.Console.WriteLine("");
+                }
+            }
         }
 
         // Generate a function with the first N harmonics of f0.
--- Tests/Test.cs
+++ Tests/Test.cs
@@ -11,93 +11,152 @@ namespace Tests
 {
     internal class Test
     {
-        private static readonly Variable t = Component.t;
-
-        public int SampleRate = 44100;
-        public int Samples = 100000;
-        public int Oversample = 8;
-        public int Iterations = 8;
-
-        public Log Log = new ConsoleLog() { Verbosity = MessageType.Info };
-
-        public void Run(IEnumerable<string> Tests, Func<double, double> Vin)
+        private static Expression FindInput(Circuit.Circuit C)
         {
-            // This test generates the signal for the LiveSPICE 'logo'.
-            //Run("Subcircuit Trivial.schx", Vin, "V1[t]", new Expression[] { "_v15[t]", "_v11[t]" });
-            //return;
-
-            foreach (string File in Tests)
-                Run(File, Vin);
+            return C.Components.Where(i => i is Input)
+                .Select(i => Component.DependentVariable(i.Name, Component.t))
+                // If there are no inputs, just make a dummy.
+                .DefaultIfEmpty("V[t]")
+                // Require exactly one input.
+                .Single();
         }
 
-        public void Run(string FileName, Func<double, double> Vin)
-        {
-            Circuit.Circuit C = Schematic.Load(FileName, Log).Build();
-            C.Name = Path.GetFileNameWithoutExtension(FileName);
-            Expression input = C.Components.OfType<Input>().Select(i => Expression.Parse(i.Name + "[t]")).DefaultIfEmpty("V[t]").SingleOrDefault();
-            IEnumerable<Expression> outputs = C.Nodes.Select(i => i.V);
-            Run(C, Vin, input, outputs);
-        }
-
-        public void Run(Circuit.Circuit C, Func<double, double> Vin, Expression Input, IEnumerable<Expression> Plots)
+        public Dictionary<Expression, List<double>> Run(
+            Circuit.Circuit C,
+            Func<double, double> Vin,
+            int SampleRate,
+            int Samples,
+            int Oversample,
+            int Iterations,
+            Expression Input = null,
+            IEnumerable<Expression> Outputs = null)
         {
             Analysis analysis = C.Analyze();
-            TransientSolution TS = TransientSolution.Solve(analysis, (Real)1 / (SampleRate * Oversample), Log);
+            TransientSolution TS = TransientSolution.Solve(analysis, (Real)1 / (SampleRate * Oversample));
+
+            // By default, pass Vin to each input of the circuit.
+            if (Input == null)
+                Input = C.Components.Where(i => i is Input)
+                    .Select(i => Component.DependentVariable(i.Name, Component.t))
+                    // If there are no inputs, just make a dummy.
+                    .DefaultIfEmpty("V[t]")
+                    // Require exactly one input.
+                    .Single();
+
+            // By default, produce every node of the circuit as output.
+            if (Outputs == null)
+                Outputs = C.Nodes.Select(i => i.V);
 
             Simulation S = new Simulation(TS)
             {
                 Oversample = Oversample,
                 Iterations = Iterations,
-                Log = Log,
                 Input = new[] { Input },
-                Output = Plots,
+                Output = Outputs,
             };
 
-            if (Samples > 0)
-                RunTest(S, Vin, Samples, C.Name);
-        }
+            Dictionary<Expression, List<double>> outputs = 
+                S.Output.ToDictionary(i => i, i => new List<double>(Samples));
 
-        public void RunTest(Simulation S, Func<double, double> Vin, int Samples, string Name)
-        {
             double T = S.TimeStep;
-            int t0 = 0;
-            int t1 = SampleRate / 10;
-
-            List<List<double>> output = S.Output.Select(i => new List<double>(Samples)).ToList();
-
             double t = 0;
             Random rng = new Random();
-            while (Samples > 0)
+            int remaining = Samples;
+            while (remaining > 0)
             {
-                int N = Math.Min(Samples, rng.Next(1000, 10000));
-                double[] input = new double[N];
-                List<double[]> buffers = S.Output.Select(i => new double[N]).ToList();
+                // Using a varying number of samples on each call to S.Run
+                int N = Math.Min(remaining, rng.Next(1000, 10000));
+                double[] inputBuffer = new double[N];
+                List<double[]> outputBuffers = S.Output.Select(i => new double[N]).ToList();
                 for (int n = 0; n < N; ++n, t += T)
-                    input[n] = Vin(t);
+                    inputBuffer[n] = Vin(t);
 
-                S.Run(input, buffers);
+                S.Run(inputBuffer, outputBuffers);
 
                 for (int i = 0; i < S.Output.Count(); ++i)
-                    output[i].AddRange(buffers[i]);
+                    outputs[S.Output.ElementAt(i)].AddRange(outputBuffers[i]);
 
-                Samples -= N;
+                remaining -= N;
             }
 
+            return outputs;
+        }
+
+        /// <summary>
+        /// Benchmark a circuit simulation.
+        /// By default, benchmarks producing the sum of all output components.
+        /// </summary>
+        /// <returns>The rate at which the circuit simulated, in samples per second.</returns>
+        public double Benchmark(
+            Circuit.Circuit C,
+            Func<double, double> Vin,
+            int SampleRate,
+            int Oversample,
+            int Iterations,
+            Expression Input = null,
+            IEnumerable<Expression> Outputs = null)
+        {
+            Analysis analysis = C.Analyze();
+            TransientSolution TS = TransientSolution.Solve(analysis, (Real)1 / (SampleRate * Oversample));
+
+            // By default, pass Vin to each input of the circuit.
+            if (Input == null)
+                Input = FindInput(C);
+
+            // By default, produce every node of the circuit as output.
+            if (Outputs == null)
+            {
+                Expression sum = 0;
+                foreach (Speaker i in C.Components.Where(i => i is Speaker))
+                    sum += Component.DependentVariable(i.Name, Component.t);
+                Outputs = new[] { sum };
+            }
+
+            Simulation S = new Simulation(TS)
+            {
+                Oversample = Oversample,
+                Iterations = Iterations,
+                Input = new[] { Input },
+                Output = Outputs,
+            };
+
+            int N = 1000;
+            double[] inputBuffer = new double[N];
+            List<double[]> outputBuffers = Outputs.Select(i => new double[N]).ToList();
+
+            DateTime begin = DateTime.Now;
+            double T = 1.0 / SampleRate;
+            double t = 0;
+            int samples = 0;
+            while ((DateTime.Now - begin).TotalSeconds < 3)
+            {
+                // This is counting the cost of evaluating Vin during benchmarking...
+                for (int n = 0; n < N; ++n, t += T)
+                    inputBuffer[n] = Vin(t);
+
+                S.Run(inputBuffer, outputBuffers);
+                samples += N;
+            }
+
+            return samples / (DateTime.Now - begin).TotalSeconds;
+        }
+
+        public void PlotAll(string Title, Dictionary<Expression, List<double>> Outputs)
+        {
             Plot p = new Plot()
             {
-                Title = Name,
+                Title = Title,
                 Width = 800,
                 Height = 400,
-                x0 = T * t0,
-                x1 = T * t1,
+                x0 = 0,
+                x1 = Outputs.Max(i => i.Value.Count),
                 xLabel = "Time (s)",
                 yLabel = "Voltage (V)",
             };
 
-            p.Series.AddRange(output.Select((i, j) => new Scatter(
-                i.Take(t1)
-                .Select((k, n) => new KeyValuePair<double, double>(n * T, k)).ToArray())
-            { Name = S.Output.ElementAt(j).ToString() }));
+            p.Series.AddRange(Outputs.Select(i => new Scatter(
+                i.Value.Select((k, n) => new KeyValuePair<double, double>(n, k)).ToArray())
+            { Name = i.Key.ToString() }));
         }
     }
 }
