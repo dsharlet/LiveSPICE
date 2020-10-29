2da5c134d86b7057717433be6870e17c45c7939d
Dillon <dsharlet@gmail.com>
2020-10-28 23:38:43 -0700

    Refactor triodes to allow switching models. Fixes #83

--- Circuit/Components/Triode.cs
+++ Circuit/Components/Triode.cs
@@ -5,11 +5,55 @@ using System.ComponentModel;
 
 namespace Circuit
 {
+    public enum TriodeModel
+    {
+        ChildLangmuir,
+        Koren,
+    }
+
     /// <summary>
     /// Base class for a triode.
     /// </summary>
-    public abstract class Triode : Component
+    [Category("Vacuum Tubes")]
+    [DisplayName("Triode")]
+    public class Triode : Component
     {
+        protected TriodeModel model = TriodeModel.ChildLangmuir;
+        [Serialize, Description("Model implementation to use")]
+        public TriodeModel Model { get { return model; } set { model = value; NotifyChanged("Model"); } }
+
+        private double mu = 100.0;
+        [Serialize, Description("Voltage gain.")]
+        public double Mu { get { return mu; } set { mu = value; NotifyChanged("Mu"); } }
+
+        protected double k_ = 1.73e-6;
+        [Serialize, Description("Generalized perveance."), CategoryAttribute("Child-Langmuir")]
+        public double K { get { return k_; } set { k_ = value; NotifyChanged("K"); } }
+
+        private double ex = 1.4;
+        [Serialize, CategoryAttribute("Koren")]
+        public double Ex { get { return ex; } set { ex = value; NotifyChanged("Ex"); } }
+
+        private double kg = 1060.0;
+        [Serialize, CategoryAttribute("Koren")]
+        public double Kg { get { return kg; } set { kg = value; NotifyChanged("Kg"); } }
+
+        private double kp = 600.0;
+        [Serialize, CategoryAttribute("Koren")]
+        public double Kp { get { return kp; } set { kp = value; NotifyChanged("Kp"); } }
+
+        private double kvb = 300;
+        [Serialize, CategoryAttribute("Koren")]
+        public double Kvb { get { return kvb; } set { kvb = value; NotifyChanged("Kvb"); } }
+
+        private Quantity rgk = new Quantity(1e6, Units.Ohm);
+        [Serialize, CategoryAttribute("Koren")]
+        public Quantity Rgk { get { return rgk; } set { if (rgk.Set(value)) NotifyChanged("Rgk"); } }
+
+        private Quantity vg = new Quantity(0.33, Units.V);
+        [Serialize, CategoryAttribute("Koren")]
+        public Quantity Vg { get { return vg; } set { if (vg.Set(value)) NotifyChanged("Vg"); } }
+
         private Terminal p, g, k;
         public override IEnumerable<Terminal> Terminals
         {
@@ -35,15 +79,27 @@ namespace Circuit
             Name = "V1";
         }
 
-        protected abstract void Analyze(Analysis Mna, Expression Vgk, Expression Vpk, out Expression ip, out Expression ig);
-
         public override void Analyze(Analysis Mna)
         {
             Expression Vpk = Mna.AddUnknownEqualTo(Name + "pk", p.V - k.V);
             Expression Vgk = Mna.AddUnknownEqualTo(Name + "gk", g.V - k.V);
 
             Expression ip, ig;
-            Analyze(Mna, Vgk, Vpk, out ip, out ig);
+            switch (model)
+            {
+                case TriodeModel.ChildLangmuir:
+                    Expression Ed = Mu * Vgk + Vpk;
+                    ip = Call.If(Ed > 0, K * (Ed ^ 1.5), 0);
+                    ig = 0;
+                    break;
+                case TriodeModel.Koren:
+                    Expression E1 = Ln1Exp(Kp * (1.0 / Mu + Vgk * (Kvb + Vpk ^ 2) ^ (-0.5))) * Vpk / Kp;
+                    ip = (Call.Max(E1, 0) ^ Ex) / Kg;
+                    ig = Call.Max(Vgk - Vg, 0) / Rgk;
+                    break;
+                default:
+                    throw new NotImplementedException("Triode model " + model.ToString());
+            }
             ip = Mna.AddUnknownEqualTo("i" + Name + "p", ip);
             ig = Mna.AddUnknownEqualTo("i" + Name + "g", ig);
             Mna.AddTerminal(p, ip);
@@ -77,5 +133,24 @@ namespace Circuit
         }
 
         public override void LayoutSymbol(SymbolLayout Sym) { LayoutSymbol(Sym, p, g, k, () => Name, () => PartNumber); }
+
+        // ln(1+e^x) = x for large x, and large x causes numerical issues.
+        private static Expression Ln1Exp(Expression x)
+        {
+            return Call.If(x > 50, x, Call.Ln(1 + Call.Exp(x)));
+        }
+    }
+
+    // Deprecated triode classes.
+    [Obsolete]
+    public class KorenTriode : Triode
+    {
+        public KorenTriode() { model = TriodeModel.Koren; }
+    }
+
+    [Obsolete]
+    public class ChildLangmuirTriode : Triode
+    {
+        public ChildLangmuirTriode() { model = TriodeModel.ChildLangmuir; }
     }
 }
