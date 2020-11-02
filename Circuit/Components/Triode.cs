f4d4dbd1ca8acd411879ba3ba01e09752280ad27
agas <08fedra@interia.pl>
2020-11-02 21:04:45 +0100

    Added Dempwolf/Zolzer triode model

--- Circuit/Components/Triode.cs
+++ Circuit/Components/Triode.cs
@@ -9,6 +9,7 @@ namespace Circuit
     {
         ChildLangmuir,
         Koren,
+        DempwolfZolzer
     }
 
     /// <summary>
@@ -18,7 +19,7 @@ namespace Circuit
     [DisplayName("Triode")]
     public class Triode : Component
     {
-        protected TriodeModel model = TriodeModel.ChildLangmuir;
+        protected TriodeModel model;
         [Serialize, Description("Model implementation to use")]
         public TriodeModel Model { get { return model; } set { model = value; NotifyChanged("Model"); } }
 
@@ -27,32 +28,60 @@ namespace Circuit
         public double Mu { get { return mu; } set { mu = value; NotifyChanged("Mu"); } }
 
         protected double k_ = 1.73e-6;
-        [Serialize, Description("Generalized perveance."), CategoryAttribute("Child-Langmuir")]
+        [Serialize, Description("Generalized perveance."), Category("Child-Langmuir")]
         public double K { get { return k_; } set { k_ = value; NotifyChanged("K"); } }
 
         private double ex = 1.4;
-        [Serialize, CategoryAttribute("Koren")]
+        [Serialize, Category("Koren")]
         public double Ex { get { return ex; } set { ex = value; NotifyChanged("Ex"); } }
 
         private double kg = 1060.0;
-        [Serialize, CategoryAttribute("Koren")]
+        [Serialize, Category("Koren")]
         public double Kg { get { return kg; } set { kg = value; NotifyChanged("Kg"); } }
 
         private double kp = 600.0;
-        [Serialize, CategoryAttribute("Koren")]
+        [Serialize, Category("Koren")]
         public double Kp { get { return kp; } set { kp = value; NotifyChanged("Kp"); } }
 
         private double kvb = 300;
-        [Serialize, CategoryAttribute("Koren")]
+        [Serialize, Category("Koren")]
         public double Kvb { get { return kvb; } set { kvb = value; NotifyChanged("Kvb"); } }
 
         private Quantity rgk = new Quantity(1e6, Units.Ohm);
-        [Serialize, CategoryAttribute("Koren")]
+        [Serialize, Category("Koren")]
         public Quantity Rgk { get { return rgk; } set { if (rgk.Set(value)) NotifyChanged("Rgk"); } }
 
         private Quantity vg = new Quantity(0.33, Units.V);
-        [Serialize, CategoryAttribute("Koren")]
+        [Serialize, Category("Koren")]
         public Quantity Vg { get { return vg; } set { if (vg.Set(value)) NotifyChanged("Vg"); } }
+
+        private double gamma = 1.26;
+        [Serialize, Category("Dempwolf/Zolzer")]
+        public double Gamma { get { return gamma; } set { gamma = value; NotifyChanged(nameof(Gamma)); } }
+
+        private double _g = 2.242E-3;
+        [Serialize, Category("Dempwolf/Zolzer")]
+        public double G { get { return _g; } set { _g = value; NotifyChanged(nameof(G)); } }
+
+        private double gg = 6.177E-4;
+        [Serialize, Category("Dempwolf/Zolzer")]
+        public double Gg { get { return gg; } set { gg = value; NotifyChanged(nameof(Gg)); } }
+
+        private double c = 3.4;
+        [Serialize, Category("Dempwolf/Zolzer")]
+        public double C { get { return c; } set { c = value; NotifyChanged(nameof(C)); } }
+
+        private double cg = 9.901;
+        [Serialize, Category("Dempwolf")]
+        public double Cg { get { return cg; } set { cg = value; NotifyChanged(nameof(Cg)); } }
+
+        private double xi = 1.314;
+        [Serialize, Category("Dempwolf/Zolzer")]
+        public double Xi { get { return xi; } set { xi = value; NotifyChanged(nameof(Xi)); } }
+
+        private Quantity ig0 = new Quantity(8.025E-8, Units.A);
+        [Serialize, Category("Dempwolf/Zolzer")]
+        public Quantity Ig0 { get { return ig0; } set { ig0 = value; NotifyChanged(nameof(Ig0)); } }
 
         private Terminal p, g, k;
         public override IEnumerable<Terminal> Terminals
@@ -96,6 +125,13 @@ namespace Circuit
                     Expression E1 = Ln1Exp(Kp * (1.0 / Mu + Vgk * (Kvb + Vpk ^ 2) ^ (-0.5))) * Vpk / Kp;
                     ip = (Call.Max(E1, 0) ^ Ex) / Kg;
                     ig = Call.Max(Vgk - Vg, 0) / Rgk;
+                    break;
+                case TriodeModel.DempwolfZolzer:
+                    var cgVgk = Cg * Vgk;
+                    ig = Call.If(Vgk > -5, Gg * Binary.Power(Call.If(cgVgk < 5, Call.Ln(1 + LinExp(cgVgk)), cgVgk) / Cg, Xi), 0) + Ig0;
+                    Expression ex = C * ((Vpk / Mu) + Vgk);
+                    var ik = Call.If(ex > -5, G * Binary.Power(Call.If(ex < 5, Call.Ln(1 + LinExp(ex)), ex) / C, Gamma), 0);
+                    ip = ik - ig;
                     break;
                 default:
                     throw new NotImplementedException("Triode model " + model.ToString());
