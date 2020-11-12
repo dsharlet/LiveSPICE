004ac00a8803d11b39709254ff012ddf288f82c8
agas <08fedra@interia.pl>
2020-11-12 23:36:09 +0100

    LiveSpice dependencies updated.

--- LiveSPICE/LiveSimulation.xaml.cs
+++ LiveSPICE/LiveSimulation.xaml.cs
@@ -11,9 +11,9 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
+using AvalonDock.Layout;
 using SchematicControls;
 using Util;
-using Xceed.Wpf.AvalonDock.Layout;
 
 namespace LiveSPICE
 {
