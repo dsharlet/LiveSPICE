e7825594948195531bce1064d6048cc867169797
agas <08fedra@interia.pl>
2021-03-26 13:56:38 +0100

    color converter refactoring

--- LiveSPICEVst/Converters/IntegerToBrushConverter.cs
+++ LiveSPICEVst/Converters/IntegerToBrushConverter.cs
@@ -6,22 +6,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Media;
+using StandardBrushes = System.Windows.Media.Brushes;
 
 namespace LiveSPICEVst.Converters
 {
-    class PositionToColorConverter : IValueConverter
+    class IntegerToBrushConverter : IValueConverter
     {
         public List<Brush> Brushes { get; } = new List<Brush>();
 
-        public Brush Default { get; set; }
+        public Brush Default { get; set; } = StandardBrushes.Gray;
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is int position)
-            {
-                return Brushes[position];
-            }
-            return Default;
+            return value is int position && position < Brushes.Count ? Brushes[position] : Default;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
