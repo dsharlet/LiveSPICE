1b6663c14d8e9f858e4a1701053077eead05e7f3
agas <08fedra@interia.pl>
2021-08-14 17:34:31 +0200

    fixes

---
+++ Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
