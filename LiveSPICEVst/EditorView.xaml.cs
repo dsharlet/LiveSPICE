da4f41bce3f1123ac8ebfbe31de435fc2cceefa8
Kostoski Stefan <kostoski.stefan90@gmail.com>
2024-01-30 14:58:47 +0100

    Add auto-reload feature

--- LiveSPICEVst/EditorView.xaml.cs
+++ LiveSPICEVst/EditorView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.IO;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -15,6 +16,9 @@ namespace LiveSPICEVst
 
         SchematicWindow schematicWindow = null;
 
+        FileSystemWatcher loadedCircuitFileWatcher = null;
+        readonly SynchronizationContext uiThreadContext = null;
+
         public EditorView(LiveSPICEPlugin plugin)
         {
             this.Plugin = plugin;
@@ -23,6 +27,7 @@ namespace LiveSPICEVst
             Plugin.EditorView = this;
 
             InitializeComponent();
+            uiThreadContext = SynchronizationContext.Current;
 
             UpdateSchematic();
         }
@@ -88,6 +93,8 @@ namespace LiveSPICEVst
                 Plugin.LoadSchematic(dialog.FileName);
 
                 UpdateSchematic();
+
+                AutoReloadSetup();
             }
         }
 
@@ -99,16 +106,8 @@ namespace LiveSPICEVst
 
         private void ReloadCircuitButton_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(Plugin.SchematicPath))
-            {
-                return;
-            }
-
-            Plugin.LoadSchematic(Plugin.SchematicPath);
-
-            UpdateSchematic();
+            ReloadCircuit();
         }
-
 
         private void ShowCircuitButton_Click(object sender, RoutedEventArgs e)
         {
@@ -126,6 +125,89 @@ namespace LiveSPICEVst
                 schematicWindow.Show();
                 schematicWindow.Activate();
             }
+        }
+
+        private void ReloadCircuit()
+        {
+            if (string.IsNullOrEmpty(Plugin.SchematicPath))
+            {
+                return;
+            }
+
+            Plugin.LoadSchematic(Plugin.SchematicPath);
+
+            UpdateSchematic();
+        }
+
+        private void OnCircuitFileUpdated(object sender, FileSystemEventArgs e)
+        {
+            // The circuit file remains locked by the OS for a short period of time after writing.
+            // A workaround for this problem is to wait for a short amount of time and keep checking
+            // if the file has been unlocked, or if a timeout has occured
+            // The timeout time is calculated as follows: delay * numberOfRetries
+            // and it this case it is 50 * 20 = 1000 milliseconds or 1 second
+            const int delay = 50;
+            int numberOfRetries = 20;
+
+            while (numberOfRetries > 0)
+            {
+                try
+                {
+                    // If the file can be successfully opened,
+                    // it means that it's unlocked and the circuit can be reloaded
+                    FileStream circuitFile = File.Open(
+                        Plugin.SchematicPath, 
+                        FileMode.Open, 
+                        FileAccess.ReadWrite, 
+                        FileShare.None);
+
+                    circuitFile.Close();
+
+                    // The OnCircuitFileUpdated function is called a from a thread separate from the UI
+                    // and it cannot directly access the plugin data.
+                    // Therefore, the main UI thread should be notified to call the reload function.
+                    uiThreadContext.Send(x => ReloadCircuit(), null);
+                    return;
+                }
+                catch { }
+
+                Thread.Sleep(delay);
+                --numberOfRetries;
+            }
+        }
+
+        private void AutoReloadCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            AutoReloadSetup();
+        }
+
+        private void AutoReloadSetup()
+        {
+            // Dispose of the resources used by the file watcher (if needed) when the checkbox is unchecked
+            if (AutoReloadCheckBox.IsChecked == false)
+            {
+                loadedCircuitFileWatcher?.Dispose();
+                return;
+            }
+
+            // If a schematic is not loaded, there is no file to be watched
+            if (string.IsNullOrEmpty(Plugin.SchematicPath))
+            {
+                return;
+            }
+
+            // Create a new File System Watcher to watch for write events on the loaded schematic file
+            loadedCircuitFileWatcher = new FileSystemWatcher
+            {
+                Filter = Path.GetFileName(Plugin.SchematicPath),
+                Path = Path.GetDirectoryName(Plugin.SchematicPath),
+                NotifyFilter = NotifyFilters.LastWrite,
+                // EnableRaisingEvents has to be set last, otherwise it throws an error because the path is still empty
+                EnableRaisingEvents = true,
+            };
+
+            // Link a callback function to the file watcher that executes each time a new write operation occurs
+            loadedCircuitFileWatcher.Changed += OnCircuitFileUpdated;
         }
     }
 }
