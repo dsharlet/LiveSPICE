5dda7b729660f1764166472020c8cc656bff9e4d
Kostoski Stefan <33221623+kostoskistefan@users.noreply.github.com>
2024-02-01 17:42:35 +0100

    Add an auto reload feature (#217)
    * Add auto-reload feature
    
    * Fix Auto-Reload nonoptimal file reload and implement rename listener
    
    * Remove redundant code
    
    * Remove auto-reload on manual file rename and optimize code

--- LiveSPICEVst/EditorView.xaml.cs
+++ LiveSPICEVst/EditorView.xaml.cs
@@ -15,6 +15,8 @@ namespace LiveSPICEVst
 
         SchematicWindow schematicWindow = null;
 
+        FileSystemWatcher loadedCircuitFileWatcher = null;
+
         public EditorView(LiveSPICEPlugin plugin)
         {
             this.Plugin = plugin;
@@ -85,9 +87,8 @@ namespace LiveSPICEVst
 
             if (dialog.ShowDialog() == true)
             {
-                Plugin.LoadSchematic(dialog.FileName);
-
-                UpdateSchematic();
+                ReloadCircuit(dialog.FileName);
+                AutoReloadSetup();
             }
         }
 
@@ -99,16 +100,8 @@ namespace LiveSPICEVst
 
         private void ReloadCircuitButton_Click(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(Plugin.SchematicPath))
-            {
-                return;
-            }
-
-            Plugin.LoadSchematic(Plugin.SchematicPath);
-
-            UpdateSchematic();
+            ReloadCircuit(Plugin.SchematicPath);
         }
-
 
         private void ShowCircuitButton_Click(object sender, RoutedEventArgs e)
         {
@@ -126,6 +119,67 @@ namespace LiveSPICEVst
                 schematicWindow.Show();
                 schematicWindow.Activate();
             }
+        }
+
+        private void ReloadCircuit(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                return;
+            }
+
+            Plugin.LoadSchematic(path);
+            UpdateSchematic();
+        }
+
+        private void OnCircuitFileUpdate(object sender, FileSystemEventArgs e)
+        {
+            if (e.FullPath != Plugin.SchematicPath)
+            {
+                return;
+            }
+
+            // The OnCircuitFileUpdate function is called a from a thread separate from the UI
+            // and it cannot directly access the plugin data.
+            // Therefore, the main UI thread should be notified to call the reload function.
+            Dispatcher.Invoke(() => ReloadCircuit(Plugin.SchematicPath));
+        }
+
+        private void AutoReloadCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            AutoReloadSetup();
+        }
+
+        private void AutoReloadSetup()
+        {
+            // Dispose of the resources used by the file watcher (if needed) when the checkbox is unchecked
+            if (AutoReloadCheckBox.IsChecked == false)
+            {
+                loadedCircuitFileWatcher?.Dispose();
+                return;
+            }
+
+            // If a schematic is not loaded, there is no file to be watched
+            if (string.IsNullOrEmpty(Plugin.SchematicPath))
+            {
+                return;
+            }
+
+            // Create a new File System Watcher to watch for write events on the loaded schematic file
+            loadedCircuitFileWatcher = new FileSystemWatcher
+            {
+                Filter = Path.GetFileName(Plugin.SchematicPath),
+                Path = Path.GetDirectoryName(Plugin.SchematicPath),
+                NotifyFilter = NotifyFilters.FileName,
+                // EnableRaisingEvents has to be set last, otherwise it throws an error because the path is still empty
+                EnableRaisingEvents = true,
+            };
+
+            // Link a callback function to the file watcher that executes
+            // each time a rename operation occurs on the loaded schematic file.
+            // A rename happens because whenever the schematic file is saved, it is firstly
+            // stored as a .temp file and after writing, the file is renamed to use the correct extension.
+            loadedCircuitFileWatcher.Renamed += OnCircuitFileUpdate;
         }
     }
 }
